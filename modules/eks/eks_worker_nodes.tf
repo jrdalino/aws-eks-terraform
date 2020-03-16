e9783fb1d35e1d9de7ee648ecb2b02378f7f7df8
Jose Dee Dalino Jr <Jose.Dalino@excelian.com>
2020-03-16 21:27:26 +0800

    Initial

--- modules/eks/eks_worker_nodes.tf
+++ modules/eks/eks_worker_nodes.tf
@@ -36,6 +36,12 @@ resource "aws_iam_role_policy_attachment" "ecr_policy" {
   role       = aws_iam_role.node_group.name
 }
 
+# IAM Policy Attachment - DynamoDB Policy
+resource "aws_iam_role_policy_attachment" "dynamodb_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+  role       = aws_iam_role.node_group.name
+}
+
 # Node Group
 resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
